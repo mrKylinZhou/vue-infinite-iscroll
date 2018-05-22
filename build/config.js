4bf5c93567df5629add7cbfe556a72cd69db59ac
Kylin <mrkylin.zhou@gmail.com>
2018-05-22 16:36:12 +0800

    webpack init

--- build/config.js
+++ build/config.js
@@ -1,3 +1,7 @@
-module.exports = {
+const vueLoaderConfig = {
 
+}
+
+module.exports = {
+  vueLoaderConfig: vueLoaderConfig
 }
--- build/webpack.js
+++ build/webpack.js
@@ -1,1 +1,30 @@
+const path = require('path')
+
 const config = require('./config')
+
+const vueLoaderConfig = config.vueLoaderConfig
+
+module.exports = {
+  context: path.join(__dirname, '../'),
+  entry: './src/main.js',
+  output: {
+
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: vueLoaderConfig
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [resolve('src')]
+      }
+    ]
+  }
+}
