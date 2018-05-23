bb081e2dfb2d625e658543498ee73e04800e79d8
Kylin <mrkylin.zhou@gmail.com>
2018-05-23 11:44:47 +0800

    代码优化 & 自定义填充

--- src/scroll/Index.vue
+++ src/scroll/Index.vue
@@ -1,38 +1,63 @@
 <template>
-  <div>
-    <scroll-wrap
-      :lists="lists"
-      @update="handleUpdate">
+  <div
+    ref="wrapper"
+    class="wrapper">
+    <div class="scroller">
       <div
+        ref="row"
         class="row"
         v-for="(item, index) in showLists"
         :key="index"
         :data-id="item">
-        {{ item }}
+        <slot :data="item"></slot>
       </div>
-    </scroll-wrap>
+    </div>
   </div>
 </template>
 
 <script>
-import ScrollWrap from './Wrap'
+import IScroll from 'iscroll/build/iscroll-infinite'
 export default {
-  data() {
-    const lists = new Array(2000).fill('t').map((item, i) => item + i)
-    const checkboxs = Object.create(null)
-    lists.forEach(item => {
-      checkboxs[item] = true
-    })
-    return {
-      lists,
-      showLists: lists.slice(0, 30),
-      checkboxs
+  props: {
+    lists: {
+      required: true,
+      type: Array
     }
   },
-  components: {
-    ScrollWrap
+  data() {
+    return {
+      showLists: this.lists.slice(0, 30)
+    }
+  },
+  watch: {
+    lists() {
+      this.scroll & this.scroll.destroy()
+      this.scroll = null
+      this.$nextTick(() => {
+        this.$init()
+      })
+    }
   },
   methods: {
+    $init() {
+      this.scroll = new IScroll(this.$refs.wrapper, {
+        mouseWheel: true,
+        infiniteElements: this.$refs.row,
+        dataset: this.requestData,
+        dataFiller: this.updateContent,
+        infiniteLimit: this.lists.length,
+        cacheSize: 1000
+      });
+    },
+    requestData(start, count) {
+      this.$nextTick(() => {
+        this.scroll && this.scroll.updateCache(start, this.lists.slice(start, start + count))
+      })
+    },
+    updateContent(el, value) {
+      const oldVal= el.getAttribute('data-id')
+      this.handleUpdate(oldVal, value)
+    },
     handleUpdate(oldVal, value) {
       if (oldVal === value) return
       const keyIndex = this.showLists.findIndex(item => item === oldVal)
@@ -42,6 +67,41 @@ export default {
         this.$forceUpdate()
       })
     }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.$init()
+    })
   }
 }
 </script>
+
+<style scoped>
+  .wrapper {
+    position: relative;
+    max-height: 300px;
+    z-index: 1;
+    overflow: hidden;
+  }
+
+  .scroller {
+    position: absolute;
+    z-index: 1;
+    width: 100%;
+    transform: translateZ(0);
+    user-select: none;
+  }
+
+  .scroller {
+    padding: 0;
+    margin: 0;
+    position: relative;
+  }
+
+  .scroller .row {
+    position: absolute;
+    top: 0;
+    left: 0;
+    transform: translateZ(0);
+  }
+</style>
